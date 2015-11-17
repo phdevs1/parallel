147538ff929fa4339fce16329f483e0e1ac39578
gma@gmail.com <lab>
2015-11-17 07:42:58 -0500

    correct

--- openmp/exe5.c
+++ openmp/exe5.c
@@ -9,11 +9,16 @@ int main(int argc, char* argv[])
 
    float a[] = {4.0,3.0,3.0,1000.0};
    int i;
-   float sum = 0.0;
+   float sumc = 0.0;
+   for (i = 0; i < 4; i++)
+      sumc += a[i];
+   printf("sin pragma sum = %4.1f\n",sumc);
+
+	float sum = 0.0;
    # pragma omp parallel for num_threads(2) reduction(+:sum)
    for (i = 0; i < 4; i++)
       sum += a[i];
-   printf("sum = %4.1f\n",sum);
+   printf("con pragma sum = %4.1f\n",sum);
 
    return 0;
 }
